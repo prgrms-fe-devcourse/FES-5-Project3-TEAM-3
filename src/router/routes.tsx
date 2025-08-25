b80f1de51e17ad26a001e6d2589e0b2239609be0
sikchang <changsik323@gmail.com>
2025-08-25 09:15:11 +0900

    [feat] /community/write 페이지 로드

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,5 +1,6 @@
 import { lazy } from "react";
 import { createBrowserRouter } from "react-router";
+import CommunityWrite from "@/pages/community/CommunityWrite";
 
 /* Code Splitting */
 const Root = lazy(() => import('@/pages'))
@@ -9,7 +10,10 @@ export const routes = createBrowserRouter([
     path: '/',
     Component: Root,
     children: [
-      
-    ]
-  }
-])
+      {
+        path: 'community/write',
+        element: <CommunityWrite />,
+      },
+    ],
+  },
+]);
