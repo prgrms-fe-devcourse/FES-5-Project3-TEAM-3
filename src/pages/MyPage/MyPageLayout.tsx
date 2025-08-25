ce976a07eefb7d4048c56c1556d5f75fdce13a94
EUNJIN <eafiseemn@gmail.com>
2025-08-25 12:30:04 +0900

    [Feat] 공통 Button Component CVA로 스타일 지정

--- src/pages/MyPage/MyPageLayout.tsx
+++ src/pages/MyPage/MyPageLayout.tsx
@@ -1,16 +1,42 @@
+import Button from "@/component/Button"
 import Spinner from "@/component/Spinner"
-import { Outlet } from "react-router"
+import { Outlet, useNavigate } from "react-router"
 
 function MyPageLayout() {
+  const navigate = useNavigate();
+
   return (
-    <div className="myPageContainer w-full h-screen flex justify-center items-center ">
-      <div className="w-60 h-60 border border-gray-500 p-4 overflow-y-auto">
-        <h1>Scroll Test</h1>
-        <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident molestiae expedita sint odio cumque aliquid vero ratione facere nam! Exercitationem laudantium officiis porro voluptas nulla inventore dolore sapiente natus iusto.</p>
-      </div>
-      <div className="w-60 h-60 border border-gray-500 p-4 overflow-y-auto">
+    <div className="myPageContainer w-full h-screen flex justify-center items-center flex-col gap-4 ">
+      <div className="w-60 border border-gray-500 p-4">
         <h1>Spinner Test</h1>
         <Spinner size="80px" />
+      </div>
+      <div className="border border-gray-500 p-4 overflow-y-auto flex flex-col gap-2">
+        <h1>Button Test</h1>
+        <Button size="lg" borderType="solid" color="primary" onClick={() => navigate("/")}>Button</Button>
+        <Button size="md" borderType="solid" color="primary">Button</Button>
+        <Button size="sm" borderType="solid" color="primary">Button</Button>
+        <Button size="lg" borderType="solid" color="primary" disabled>Button</Button>
+        <Button size="md" borderType="solid" color="primary" disabled>Button</Button>
+        <Button size="sm" borderType="solid" color="primary" disabled>Button</Button>
+        <Button size="lg" borderType="outline" color="primary">Button</Button>
+        <Button size="md" borderType="outline" color="primary">Button</Button>
+        <Button size="sm" borderType="outline" color="primary">Button</Button>
+        <Button size="lg" borderType="outline" color="primary" disabled>Button</Button>
+        <Button size="md" borderType="outline" color="primary" disabled>Button</Button>
+        <Button size="sm" borderType="outline" color="primary" disabled>Button</Button>
+        <Button size="lg" borderType="solid" color="secondary">Button</Button>
+        <Button size="md" borderType="solid" color="secondary">Button</Button>
+        <Button size="sm" borderType="solid" color="secondary">Button</Button>
+        <Button size="lg" borderType="solid" color="secondary" disabled>Button</Button>
+        <Button size="md" borderType="solid" color="secondary" disabled>Button</Button>
+        <Button size="sm" borderType="solid" color="secondary" disabled>Button</Button>
+        <Button size="lg" borderType="outline" color="secondary">Button</Button>
+        <Button size="md" borderType="outline" color="secondary">Button</Button>
+        <Button size="sm" borderType="outline" color="secondary">Button</Button>
+        <Button size="lg" borderType="outline" color="secondary" disabled>Button</Button>
+        <Button size="md" borderType="outline" color="secondary" disabled>Button</Button>
+        <Button size="sm" borderType="outline" color="secondary" disabled>Button</Button>
       </div>
       <Outlet></Outlet>
     </div>
