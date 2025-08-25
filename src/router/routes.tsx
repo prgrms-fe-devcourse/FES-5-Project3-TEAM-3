313fb1a9ade7aa8f03a51b6c0c3907842eb04014
sikchang <changsik323@gmail.com>
2025-08-25 15:04:15 +0900

    [feat] Community Write 페이지 생성

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,6 +1,6 @@
 import { lazy } from "react";
 import { createBrowserRouter } from "react-router";
-import CommunityWrite from "@/pages/community/CommunityWrite";
+import CommunityWrite from "@/pages/community/write/CommunityWrite";
 
 /* Code Splitting */
 const Root = lazy(() => import('@/pages'));
