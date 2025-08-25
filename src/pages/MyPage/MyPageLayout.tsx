cacfb2370355b206118c68a2ca9604557f760327
EUNJIN <eafiseemn@gmail.com>
2025-08-25 10:49:27 +0900

    [Feat] 공통 Button Component 생성

--- src/pages/MyPage/MyPageLayout.tsx
+++ src/pages/MyPage/MyPageLayout.tsx
@@ -1,3 +1,4 @@
+import Spinner from "@/component/Spinner"
 import { Outlet } from "react-router"
 
 function MyPageLayout() {
@@ -6,6 +7,10 @@ function MyPageLayout() {
       <div className="w-60 h-60 border border-gray-500 p-4 overflow-y-auto">
         <h1>Scroll Test</h1>
         <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident molestiae expedita sint odio cumque aliquid vero ratione facere nam! Exercitationem laudantium officiis porro voluptas nulla inventore dolore sapiente natus iusto.</p>
+      </div>
+      <div className="w-60 h-60 border border-gray-500 p-4 overflow-y-auto">
+        <h1>Spinner Test</h1>
+        <Spinner size="80px" />
       </div>
       <Outlet></Outlet>
     </div>
