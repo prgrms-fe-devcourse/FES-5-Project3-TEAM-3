36784ec3dec23402e13a243e612c1df5ae34323a
EUNJIN <eafiseemn@gmail.com>
2025-08-27 00:58:23 +0900

    [Feat] Profile Image Upload Component 틀

--- src/pages/MyPage/Settings.tsx
+++ src/pages/MyPage/Settings.tsx
@@ -1,4 +1,13 @@
+import SettingImage from '@/component/MyPage/SettingImage';
+
 function Settings() {
-  return <div>Settings</div>;
+  return (
+    <div className="flex flex-col gap-12 overflow-scroll">
+      <h2 className="w-full inline-flex flex-col justify-start items-start text-2xl font-semibold">
+        Settings
+      </h2>
+      <SettingImage />
+    </div>
+  );
 }
 export default Settings;
