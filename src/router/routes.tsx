ca97c11e2fd97ad2020b14d8184af5eac77fb1c6
EUNJIN <eafiseemn@gmail.com>
2025-08-26 12:22:08 +0900

    [Feat] MyPage Nav용 Router 추가 및 router용 함수 생성

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,11 +1,19 @@
 import { lazy } from 'react';
-import { createBrowserRouter } from 'react-router';
+import { createBrowserRouter, Navigate } from 'react-router';
 import CommunityWrite from '@/pages/community/write/CommunityWrite';
 
 /* Code Splitting */
 const Root = lazy(() => import('@/pages'));
 const MainPage = lazy(() => import('@/pages/MainPage/MainPage'));
 const MyPageLayout = lazy(() => import('@/pages/MyPage/MyPageLayout'));
+
+// MyPage 하위 페이지
+const MyHome = lazy(() => import('@/pages/MyPage/MyHome'));
+const WineSeller = lazy(() => import('@/pages/MyPage/WineSeller'));
+const WishList = lazy(() => import('@/pages/MyPage/WishList'));
+const MyActivities = lazy(() => import('@/pages/MyPage/MyActivities'));
+const MyAchievement = lazy(() => import('@/pages/MyPage/MyAchievement'));
+const Settings = lazy(() => import('@/pages/MyPage/Settings'));
 
 export const routes = createBrowserRouter([
   {
@@ -24,6 +32,42 @@ export const routes = createBrowserRouter([
       {
         path: 'my-page',
         Component: MyPageLayout,
+        children: [
+          {
+            index: true,
+            element: <Navigate to="home" replace />,
+          },
+          {
+            path: 'home',
+            Component: MyHome,
+            handle: { label: 'Home', showInNav: true },
+          },
+          {
+            path: 'wine-seller',
+            Component: WineSeller,
+            handle: { label: 'Wine Seller', showInNav: true },
+          },
+          {
+            path: 'wish-list',
+            Component: WishList,
+            handle: { label: 'Wish List', showInNav: true },
+          },
+          {
+            path: 'activity',
+            Component: MyActivities,
+            handle: { label: 'My Activities', showInNav: true },
+          },
+          {
+            path: 'achievement',
+            Component: MyAchievement,
+            handle: { label: 'My Achievement', showInNav: true },
+          },
+          {
+            path: 'settings',
+            Component: Settings,
+            handle: { label: 'Settings', showInNav: true },
+          },
+        ],
       },
     ],
   },
