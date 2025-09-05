b971e5884858eaec2a520774d576834e78fd5e02
mtm1018 <mtm1018@naver.com>
2025-09-05 09:22:20 +0900

    fix/ 검색바 잗공

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import { RouterProvider } from 'react-router';
 import { routes } from './router/routes';
 import { useEffect} from 'react';
 import { useAuth } from './store/@store';
+import { useSearchStore } from './store/searchStore';
 
 function App() {
+  const {userId} = useAuth()
   useEffect(() => {
     const unsub = useAuth.getState().subscribe();
     useAuth.getState().fetch();
     return unsub;
   }, []);
 
+  useEffect(() => {
+  useSearchStore.persist.rehydrate();
+}, [userId]);
 
   return (
     <div>
