1e4e7b0222d9c77e9fdc268c889282528247358e
sikchang <changsik323@gmail.com>
2025-09-05 09:27:34 +0900

    [ fix ] 커뮤니티 상세 페이지 및 글쓰기 Preview 빈블럭 추가

--- src/pages/community/detail/CommunityDetail.tsx
+++ src/pages/community/detail/CommunityDetail.tsx
@@ -187,10 +187,19 @@ function CommunityDetail() {
             <div className="px-13 max-w-none text-gray-800">
               {/* 본문(이미 HTML로 저장된 경우) */}
               {post?.content ? (
-                <div
-                  className="prose max-w-none"
-                  dangerouslySetInnerHTML={{ __html: post.content }}
-                />
+                (() => {
+                  const content = String(post.content ?? '');
+                  // HTML 여부 판단
+                  const isHtml = /<\/?[a-z][\s\S]*>/i.test(content);
+                  if (isHtml) {
+                    const normalized = content
+                      .replace(/<p>(?:\s|&nbsp;)*<\/p>/gi, '<p><br/></p>')
+                      .replace(/<div>(?:\s|&nbsp;|<br\s*\/?>)*<\/div>/gi, '<p><br/></p>');
+                    return <div className="prose max-w-none" dangerouslySetInnerHTML={{ __html: normalized }} />;
+                  }
+                  // plain text: 개행 보존
+                  return <div className="whitespace-pre-wrap text-gray-800">{content}</div>;
+                })()
               ) : (
                 <p>본문이 없습니다.</p>
               )}
